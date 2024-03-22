7d8a934c502666cd8bdfb1d9bd2e5ce3048b2606
mohamad727 <mahmodim222@gmail.com>
2024-03-22 11:16:07 +0330

    fix main for develop

--- src/app.js
+++ src/app.js
@@ -1,26 +1,7 @@
 import { List } from './components/home/List';
-import { El } from './components/shared/El';
-import { Btn } from './components/shared/button';
 import { MainLayout } from './layout';
+
+
 export const App = () => {
-  return El({
-    element: 'div',
-    className: ' gap-10 ',
-    children: [
-      El({
-        element: 'div',
-        children: 'slam',
-        className: 'bg-red-200',
-      }),
-
-      El({
-        element: 'div',
-        children: 'slam111',
-        className: 'bg-green-400',
-      }),
-      Btn('Add To', 'border mr-5 mt-5'),
-      Btn('closed', 'border '),
-    ],
-  });
+  return MainLayout(List)
 };
-
